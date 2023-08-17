23ba75eac7c584b1bd23a6e99a304f31783c65f7
Jazzthekid <schante56@gmail.com>
2023-08-16 19:42:43 -0500

    Added Comments and Replies app and created models for both

---
+++ backend/comments/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from authentication.models import User
+
+class Comment (models.Model):
+    
+    user = models.ForeignKey (User, on_delete=models.CASCADE)
+    video_id = models.CharField (max_length=30)
+    text = models.CharField (max_length=200)
+    likes = models.IntegerField()
+    dislikes = models.IntegerField ()
+
+# class Reply (models.Model):
